a92246160067e879bd32f652d96edba80260d1d2
MH <songming05@gmail.com>
2018-12-18 10:42:03 +0900

    Java 예제 풀이
    더 짧은 코드로 작성할 수 있는 방법 추가

--- Money.java
+++ Money.java
@@ -13,8 +13,8 @@ class Money {
 	public static void main(String[] args) {
 		int money = 5679;
 		int thousands = money/1000;
-		int hundreds = (money - (thousands * 1000))/100;
-		int ten = (money - (thousands * 1000 + hundreds * 100))/10;
+		int hundreds = (money - (thousands * 1000))/100;	//money%1000/100;
+		int ten = (money - (thousands * 1000 + hundreds * 100))/10;		//money%100/10
 		int one = money%10;
 
 		System.out.println("±Ý¾× : " + money + "¿ø");
