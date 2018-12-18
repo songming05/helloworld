99003c441bbfce4905fecce3c1ae3f183e5b6da4
MH <songming05@gmail.com>
2018-12-18 10:02:38 +0900

    Java 예제 풀이
    월급계산과 거스름돈계산

---
+++ Money.java
@@ -0,0 +1,27 @@
+/*
+금액(money)이 5679원일때 다음과 같이 출력하시오
+
+[실행결과]
+금액 : 5679원
+천원 : 5장
+백원 : 6개
+십원 : 7개
+일원 : 9개
+*/
+
+class Money {
+	public static void main(String[] args) {
+		int money = 5679;
+		int thousands = money/1000;
+		int hundreds = (money - (thousands * 1000))/100;
+		int ten = (money - (thousands * 1000 + hundreds * 100))/10;
+		int one = money%10;
+
+		System.out.println("금액 : " + money + "원");
+		System.out.println("천원 : " + thousands + "장");
+		System.out.println("백원 : " + hundreds + "개");
+		System.out.println("십원 : " + ten + "개");
+		System.out.println("일원 : " + one + "개");
+
+	}
+}
